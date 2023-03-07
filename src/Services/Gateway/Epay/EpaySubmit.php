6b8bb63643770c5c91ae13d659e215ba1efeaac0
zeropanel <v2rayfkgfw@gmail.com>
2023-03-07 22:31:30 +0800

    fix

--- src/Services/Gateway/Epay/EpaySubmit.php
+++ src/Services/Gateway/Epay/EpaySubmit.php
@@ -2,46 +2,27 @@
 
 declare(strict_types=1);
 
-/**
- * Copyright (c) 2019.
- * Author:Alone88
- * Github:https://github.com/anhao
- */
-
 namespace App\Services\Gateway\Epay;
 
 final class EpaySubmit
 {
-    private $config;
-    private $gateway_header;
+    private $alipay_config;
+    private $alipay_gateway_new;
 
-    
-    public function __construct($config)
+    public function __construct($alipay_config)
     {
-        $this->config = $config;
-        $this->gateway_header = $this->config['apiurl'] . 'submit.php?';
+        $this->alipay_config = $alipay_config;
+        $this->alipay_gateway_new = $this->alipay_config['apiurl'] . 'submit.php?';
     }
 
-    /**
-     * 生成签名结果
-     *
-     * @param $para_sort 已排序要签名的数组
-     * return 签名结果字符串
-     */
     public function buildRequestMysign($para_sort)
     {
         //把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串
         $prestr = EpayTool::createLinkstring($para_sort);
 
-        return EpayTool::md5Sign($prestr, $this->config['key']);
+        return EpayTool::md5Sign($prestr, $this->alipay_config['key']);
     }
-    /**
-     * 生成要请求给支付宝的参数数组
-     *
-     * @param $para_temp 请求前的参数数组
-     *
-     * @return 要请求的参数数组
-     */
+
     public function buildRequestPara($para_temp)
     {
         //除去待签名参数数组中的空值和签名参数
@@ -55,18 +36,11 @@ final class EpaySubmit
 
         //签名结果与签名方式加入请求提交参数组中
         $para_sort['sign'] = $mysign;
-        $para_sort['sign_type'] = strtoupper(trim($this->config['sign_type']));
+        $para_sort['sign_type'] = strtoupper(trim($this->alipay_config['sign_type']));
 
         return $para_sort;
     }
 
-    /**
-     * 生成要请求给支付宝的参数数组
-     *
-     * @param $para_temp 请求前的参数数组
-     *
-     * @return 要请求的参数数组字符串
-     */
     public function buildRequestParaToString($para_temp)
     {
         //待请求参数数组
@@ -76,20 +50,11 @@ final class EpaySubmit
         return EpayTool::createLinkstringUrlencode($para);
     }
 
-    /**
-     * 建立请求，以表单HTML形式构造（默认）
-     *
-     * @param $para_temp 请求参数数组
-     * @param $method 提交方式。两个值可选：post、get
-     * @param $button_name 确认按钮显示文字
-     *
-     * @return 提交表单HTML文本
-     */
-    public function buildRequestForm($para_temp, $method = 'POST')
+    public function buildRequestForm($para_temp)
     {
         //待请求参数数组
         $para = http_build_query($this->buildRequestPara($para_temp));
-        $url = $this->gateway_header . 'method=' . $method . $para;
+        $url = $this->gateway_header . $para;
         return $url;
     }
 }
