f5703c1ff64ed578f781ac83518f2ef4940c01e1
zeropanel <v2rayfkgfw@gmail.com>
2023-03-07 21:59:43 +0800

    fix

--- src/Services/Gateway/Epay/EpaySubmit.php
+++ src/Services/Gateway/Epay/EpaySubmit.php
@@ -12,14 +12,14 @@ namespace App\Services\Gateway\Epay;
 
 final class EpaySubmit
 {
-    private $alipay_config;
-    private $alipay_gateway_new;
+    private $config;
+    private $gateway_header;
 
     
-    public function __construct($alipay_config)
+    public function __construct($config)
     {
-        $this->alipay_config = $alipay_config;
-        $this->alipay_gateway_new = $this->alipay_config['apiurl'] . 'submit.php?';
+        $this->config = $config;
+        $this->gateway_header = $this->config['apiurl'] . 'submit.php?';
     }
 
     /**
@@ -33,7 +33,7 @@ final class EpaySubmit
         //把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串
         $prestr = EpayTool::createLinkstring($para_sort);
 
-        return EpayTool::md5Sign($prestr, $this->alipay_config['key']);
+        return EpayTool::md5Sign($prestr, $this->config['key']);
     }
     /**
      * 生成要请求给支付宝的参数数组
@@ -55,7 +55,7 @@ final class EpaySubmit
 
         //签名结果与签名方式加入请求提交参数组中
         $para_sort['sign'] = $mysign;
-        $para_sort['sign_type'] = strtoupper(trim($this->alipay_config['sign_type']));
+        $para_sort['sign_type'] = strtoupper(trim($this->config['sign_type']));
 
         return $para_sort;
     }
@@ -89,7 +89,7 @@ final class EpaySubmit
     {
         //待请求参数数组
         $para = http_build_query($this->buildRequestPara($para_temp));
-        $url = $this->alipay_gateway_new . 'method=' . $method . $para;
+        $url = $this->gateway_header . 'method=' . $method . $para;
         return $url;
     }
 }
