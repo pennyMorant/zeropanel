135bc460d03aa918087ab6ca816106245a545ea8
zeropanel <v2rayfkgfw@gmail.com>
2023-03-29 01:48:57 +0800

    fix

--- src/Utils/Telegram/Commands/UnbindCommand.php
+++ src/Utils/Telegram/Commands/UnbindCommand.php
@@ -1,11 +1,8 @@
 <?php
-
 declare(strict_types=1);
 
 namespace App\Utils\Telegram\Commands;
 
-use App\Models\Setting;
-use App\Utils\Telegram\TelegramTools;
 use Telegram\Bot\Actions;
 use Telegram\Bot\Commands\Command;
 
@@ -22,76 +19,33 @@ final class UnbindCommand extends Command
     /**
      * @var string Command Description
      */
-    protected $description = '[私聊]     解除账户绑定.';
+    protected $description = '解除账户绑定';
 
-    public function handle(): void
+    public function handle()
     {
-        $Update = $this->getUpdate();
-        $Message = $Update->getMessage();
-        // 消息会话 ID
-        $ChatID = $Message->getChat()->getId();
-
-        // 触发用户
-        $SendUser = [
-            'id' => $Message->getFrom()->getId(),
-        ];
-
-        $User = TelegramTools::getUser($SendUser['id']);
-
-        if ($ChatID > 0) {
-            // 私人
-
-            // 发送 '输入中' 会话状态
-            $this->replyWithChatAction(['action' => Actions::TYPING]);
-
-            if ($User === null) {
-                // 回送信息
-                $this->replyWithMessage(
-                    [
-                        'text' => Setting::obtain('user_not_bind_reply'),
-                        'parse_mode' => 'Markdown',
-                    ]
-                );
-                return;
-            }
-
-            // 消息内容
-            $MessageText = explode(' ', trim($Message->getText()));
-            $MessageKey = array_splice($MessageText, -1)[0];
-            $text = '';
-
-            if ($MessageKey === $User->email) {
-                $temp = $User->telegramReset();
-                $text = $temp['msg'];
-                // 回送信息
-                $this->replyWithMessage(
-                    [
-                        'text' => $text,
-                        'parse_mode' => 'Markdown',
-                    ]
-                );
-                return;
-            }
-            if ($MessageKey !== '') {
-                $text = '键入的 Email 地址与您的账户不匹配.';
-            }
-            if ($MessageKey === '/unbind') {
-                $text = $this->sendtext();
-            }
-
-            // 回送信息
+        $this->replyWithChatAction(['action' => Actions::TYPING]);
+        $message = $this->getUpdate()->getMessage();
+        $messageId = $message->getMessageId();
+        $chatId = $message->getChat()->getId();
+        
+        $user = User::where('telegram_id', $chatId)->first();
+        if (is_null($user)) {
             $this->replyWithMessage(
                 [
-                    'text' => $text,
-                    'parse_mode' => 'Markdown',
+                    'text' => '您还没有绑定账号',
+                    'reply_to_message_id' => $messageId,
                 ]
             );
+            return;
         }
-    }
+        $user->telegram_id = NULL;
+        $user->save();
 
-    public function sendtext(): string
-    {
-        $text = '以 `/unbind example@qq.com` 的形式发送进行解绑.';
-        return $text;
+        // 回送信息
+        $this->replyWithMessage(
+            [
+                'text' => '您已经解除绑定',
+            ]
+        );
     }
 }
