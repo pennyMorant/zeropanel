fe16f7d706c1a19941d148c45f0170a3ffb823bb
zeropanel <v2rayfkgfw@gmail.com>
2023-04-01 00:17:24 +0800

    fix

--- src/Services/Gateway/Epay.php
+++ src/Services/Gateway/Epay.php
@@ -43,7 +43,7 @@ class Epay
 
         //请求参数
         $data = [
-            "pid" => trim($this->epay['partner']),
+            "pid" => $this->epay['partner'],
             "type" => $method == 'wechatpay' ? 'wxpay' : $method,
             "out_trade_no" => $order_no,
             "notify_url" => Setting::obtain('website_url') . "/payment/notify/epay",
