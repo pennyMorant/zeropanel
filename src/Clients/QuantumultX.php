ca6cd87218789191f6cd50c3b36430ea730a860c
zeropanel <v2rayfkgfw@gmail.com>
2023-08-24 19:50:45 +0800

    fix qx subs

--- src/Clients/QuantumultX.php
+++ src/Clients/QuantumultX.php
@@ -60,12 +60,21 @@ class QuantumultX
     {
         $ip_type = Tools::isIP($server['address']);
         $address = ($ip_type === 'v6' ? '[%s]' : '%s');
+        if ($server['security'] == 'tls') {
+            $obfs = 'over-tls';
+        } else if ($server['security'] == 'tls' && $server['net'] == 'ws') {
+            $obfs = 'wss';
+        } else if ($server['net'] == 'ws') {
+            $obfs = 'ws';
+        } else {
+            $obfs = '';
+        }
         $uri = sprintf(
             "vmess={$address}:%d, method=chacha20-poly1305, password=%s, obfs=%s, obfs-host=%s, obfs-uri=%s, fast-open=false, udp-relay=false, tag=%s\r\n",
             $server['address'],
             $server['port'],
             $server['uuid'],
-            $server['net'],
+            $obfs,
             $server['host'],
             $server['path'],
             $server['remark']
@@ -78,11 +87,14 @@ class QuantumultX
     {
         $ip_type = Tools::isIP($server['address']);
         $address = ($ip_type === 'v6' ? '[%s]' : '%s');
+        $tlsVerify = $server['insecure'] ? 'false' : 'true';
         $uri = sprintf(
-            "trojan={$address}:%d, password=%s, over-tls=true, tls-verification=true, fast-open=false, udp-relay=false, tag=%s\r\n",
+            "trojan={$address}:%d, password=%s, over-tls=true, tls-host=%s, tls-verification=%s, fast-open=false, udp-relay=false, tag=%s\r\n",
             $server['address'],
             $server['port'],
             $server['uuid'],
+            $server['host'],
+            $tlsVerify,
             $server['remark']
         );
 
